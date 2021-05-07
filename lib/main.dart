5a390ac5a83c77994ccd78542ce8025ffc7b1708
Sanambedi <55624366+Sanambedi@users.noreply.github.com>
2021-05-08 00:18:24 +0530

    origin

--- lib/main.dart
+++ lib/main.dart
@@ -66,7 +66,7 @@ class MyHomePage extends StatelessWidget {
                     ),
                     padding: EdgeInsets.all(10),
                     child: Text(
-                      tx.amount.toString(),
+                      '₹ ${tx.amount}',
                       style: TextStyle(
                         fontWeight: FontWeight.bold,
                         fontSize: 20,
