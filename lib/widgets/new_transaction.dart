9df36948e55e71a6123c57d9c01b137e956a4d24
Sanambedi <55624366+Sanambedi@users.noreply.github.com>
2021-05-14 00:23:25 +0530

    origin

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,8 +1,11 @@
 import "package:flutter/material.dart";
 
 class NewTransaction extends StatelessWidget {
+  final Function addTx;
   final titleController = TextEditingController();
   final amountController = TextEditingController();
+
+  NewTransaction(this.addTx);
 
   @override
   Widget build(BuildContext context) {
@@ -31,7 +34,10 @@ class NewTransaction extends StatelessWidget {
                 foregroundColor: MaterialStateProperty.all(Colors.blue[0]),
               ),
               onPressed: () {
-                print(titleController.text);
+                addTx(
+                  titleController.text,
+                  double.parse(amountController.text),
+                );
               },
             )
           ],
