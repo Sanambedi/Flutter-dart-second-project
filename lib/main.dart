3b9135272fabf4b0aa833d5c38fccbc771c04a26
Sanambedi <55624366+Sanambedi@users.noreply.github.com>
2021-05-15 01:34:08 +0530

    origin

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import "package:flutter/material.dart";
 import './widgets/user_transactions.dart';
 
-
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -22,24 +21,26 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("My Second App"),
-      ),
-      body: Column(
-        // mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Container(
-            width: double.infinity,
-            child: Card(
-              color: Colors.blue,
-              child: Text("Chart!"),
-              elevation: 5,
-            ),
+        appBar: AppBar(
+          title: Text("My Second App"),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            // mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                width: double.infinity,
+                child: Card(
+                  color: Colors.blue,
+                  child: Text("Chart!"),
+                  elevation: 5,
+                ),
+              ),
+              UserTransactions()
+            ],
           ),
-          UserTransactions()
-        ],
-      ),
+        )
     );
   }
 }
